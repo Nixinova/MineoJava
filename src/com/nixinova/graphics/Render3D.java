9705de78c56bc3fbc6fa25e8af988996a51b4361
Nixinova <nixinova@gmail.com>
2024-07-25 16:51:56 +1200

    Remove vestigial parts of Render3D

--- src/com/nixinova/graphics/Render3D.java
+++ src/com/nixinova/graphics/Render3D.java
@@ -4,15 +4,10 @@ import com.nixinova.main.Game;
 
 public class Render3D extends Render {
 	private BlocksRenderer blockRender;
-	private boolean fogAlrApplied;
-	private double lastXMove, lastYMove, lastZMove, lastGround, lastRot, lastTilt;
-	private boolean[] lastKbdInput;
 
 	public Render3D(int width, int height) {
 		super(width, height);
 		this.blockRender = new BlocksRenderer(width, height);
-		this.fogAlrApplied = false;
-		this.lastXMove = this.lastYMove = this.lastZMove = this.lastRot = this.lastTilt = 0;
 	}
 
 	public void renderWorld(Game game) {
