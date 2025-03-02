299dd0fd1d320486f8c67e171727a2791180abfc
Nixinova <nixinova@gmail.com>
2025-03-02 23:06:05 +1300

    Overhaul save file format

--- src/com/nixinova/mineo/main/SavedGame.java
+++ src/com/nixinova/mineo/main/SavedGame.java
@@ -20,12 +20,9 @@ public class SavedGame {
 	 * - +1.0: Major: Fundamental file format changes that make older save files unreadable.
 	 * - +0.1: Minor: Addition of new data items; keeps backward compatibility.
 	 */
-	public static float SAVE_VERSION = 1.0f;
+	public static float SAVE_VERSION = 2.0f;
 
 	private static final String saveFilePath = Mineo.rootFolder + "/" + SAVE_FILE;
-	private static final char CHAR_VERSION = 'v';
-	private static final char CHAR_PLAYERPOS = 'P';
-	private static final char CHAR_BLOCKPOS = 'B';
 
 	public World world;
 	public Player player;
@@ -33,7 +30,7 @@ public class SavedGame {
 	public SavedGame() {
 		try {
 			loadFromFile();
-		} catch (FileNotFoundException | ArrayIndexOutOfBoundsException err) {
+		} catch (FileNotFoundException | ArrayIndexOutOfBoundsException | Error err) {
 			System.err.println("Save file not found or is corrupted.");
 			this.world = new World();
 			this.player = new Player(this.world.getHorizontalCentre());
@@ -46,11 +43,11 @@ public class SavedGame {
 			FileWriter writer = new FileWriter(saveFilePath);
 			
 			// Write version
-			writer.write(String.format("%c %.1f : Mineo %s\n", CHAR_VERSION, SAVE_VERSION, Mineo.VERSION));
+			writer.write(String.format("v %.1f (Mineo %s)\n", SAVE_VERSION, Mineo.VERSION));
 
 			// Writer player position
 			TxCoord playerPos = game.player.getPosition().toTx();
-			writer.write(String.format("%c %d,%d,%d\n", CHAR_PLAYERPOS, playerPos.x, playerPos.y, playerPos.z));
+			writer.write(String.format("%c %d,%d,%d\n", 'P', playerPos.x, playerPos.y, playerPos.z));
 
 			// Write each changed block in world
 			for (int x = 0; x < World.maxCorner.x; x++) {
@@ -58,7 +55,10 @@ public class SavedGame {
 					for (int z = 0; z < World.maxCorner.z; z++) {
 						Block block = game.world.getBlockAt(x, y, z);
 						int blockId = Arrays.asList(Block.BLOCKS).indexOf(block);
-						writer.write(String.format("%c %d,%d,%d %s\n", CHAR_BLOCKPOS, x, y, z, blockId));
+						
+						long posData = x << 16 | y << 8 | z;
+						long data = posData << 8 | blockId;
+						writer.write(String.format("%x\n", data));
 					}
 				}
 			}
@@ -78,20 +78,24 @@ public class SavedGame {
 
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine().trim();
-			String[] lineParts = line.split(" ");
-			final char modeChar = lineParts[0].charAt(0);
+			final char modeChar = line.charAt(0);
 
 			switch (modeChar) {
 				// Save file version
-				case CHAR_VERSION -> {
-					float ver = Float.parseFloat(lineParts[1]);
+				case 'v' -> {
+					float ver = Float.parseFloat(line.split(" ")[1]);
 					if (ver != SAVE_VERSION) {
-						System.err.println("Save file is out of date!");
+						if (Math.floor(ver) == Math.floor(SAVE_VERSION)) {
+							System.err.println("Save file is out of date! Some data may not load correctly.");
+						} else {
+							System.err.println("Save file is critically out of date and cannot be read.");
+							throw new Error();
+						}
 					}
 				}
 				// Player position data
-				case CHAR_PLAYERPOS -> {
-					String[] posData = lineParts[1].trim().split(",");
+				case 'P' -> {
+					String[] posData = line.split(" ")[1].split(",");
 					int posX = Integer.parseInt(posData[0]);
 					int posY = Integer.parseInt(posData[1]);
 					int posZ = Integer.parseInt(posData[2]);
@@ -99,20 +103,20 @@ public class SavedGame {
 					this.player = new Player(pos);
 				}
 				// Changed blocks data
-				case CHAR_BLOCKPOS -> {
-					String[] posData = lineParts[1].trim().split(",");
-					int blockId = Integer.parseInt(lineParts[2]);
-					int posX = Integer.parseInt(posData[0]);
-					int posY = Integer.parseInt(posData[1]);
-					int posZ = Integer.parseInt(posData[2]);
+				default -> {
+					var data = Long.parseLong(line, 16);
+					int x = (int) ((data >> 24) & 0xFF);
+					int y = (int) ((data >> 16) & 0xFF);
+					int z = (int) ((data >> 8) & 0xFF);
+					int blockId = (int) (data & 0xFF);
 					Block block = Block.BLOCKS[blockId];
-					blockChanges[posX][posY][posZ] = block;
+					blockChanges[x][y][z] = block;
 				}
 			}
 		}
 		
 		this.world = new World(blockChanges);
-		
+
 		scanner.close();
 	}
 
